ae2ce41206bf283c1deae438fb924030e25d8164
Cooper Benson <skycoop@gmail.com>
2024-09-23 16:51:59 -0600

    updates from setting up new laptop

---
+++ dot_wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Darcula (base16)'
+
+config.font_size = 13
+config.line_height = 1.2
+
+return config
